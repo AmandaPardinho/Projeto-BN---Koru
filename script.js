5edc14251fc4c18e15bcc3a52ef44baf532b01d3
Hettore Eduardo <duh.barretoo@gmail.com>
2023-03-21 14:00:18 -0300

    Ajuste de cabeçalho e menu principal

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function openNav(){
+    /*alert("clicou no menu");*/
+    document.getElementById("myNav").style.width = '100%';
+}
+
+function closeNav(){
+    document.getElementById("myNav").style.width = '0%';
+}
+
+/*
+var abrirMenu = document.querySelector("#nav-button");
+abrirMenu.onclick = openNav;
+*/
