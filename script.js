21331e7cdf0b295fca1fe122dba15b0378f62cc2
Hettore Eduardo <duh.barretoo@gmail.com>
2023-03-22 11:08:52 -0300

    Deixando o menu responsivo

--- script.js
+++ script.js
@@ -8,9 +8,23 @@ function closeNav(){
     document.getElementById("myNav").style.width = '0%';
 }
 
-var botaoVoltar = document.getElementsByClassName();
-
-/*
-var abrirMenu = document.querySelector("#nav-button");
+//clicar no menu para abrir
+var abrirMenu = document.querySelector('#button-menu');
 abrirMenu.onclick = openNav;
-*/
+
+//clicar no X para fechar o menu
+var fecharMenu = document.querySelector('#fechar-menu');
+fecharMenu.onclick = closeNav;
+
+//fecha o menu quando clicar no item formulario e direciona para o mesmo
+var fecharMenuForms = document.querySelector('#forms');
+fecharMenuForms.onclick = closeNav;
+
+//fecha o menu ao clicar em redes sociais e direciona para o mesmo
+var fecharMenuRedes = document.querySelector('#social-media');
+fecharMenuRedes.onclick = closeNav;
+
+//fecha o menu ao clicar em baixar App e direcionao ao mesmo
+var fecharMenuBaixarApp = document.querySelector('#baixar-app');
+fecharMenuBaixarApp.onclick = closeNav;
+
